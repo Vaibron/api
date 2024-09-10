ed2fd8a7ddad470146beb85dc59bc95da727094f
mr.vaibron <160307500+Vaibron@users.noreply.github.com>
2024-09-10 11:57:27 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 import os
 
-DATABASE_URL = os.environ.get("DATABASE_URL") or "postgresql://user:password@host:port/database"
+DATABASE_URL = os.environ.get("DATABASE_URL") or "postgresql://postgres:5981@localhost:5432/new_db"
 BROKER_URL = os.environ.get("BROKER_URL") or "amqp://guest:guest@localhost:5672//"
